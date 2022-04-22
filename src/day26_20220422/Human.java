fc7e79ac957b1fa8a57bb07a74a47ad9abc64d56
gojihoon <anhj9095@naver.com>
2022-04-22 14:20:44 +0900

    ClassConstructor

---
+++ src/day26_20220422/Human.java
@@ -0,0 +1,14 @@
+package day26_20220422;
+
+public class Human {
+	// 이름, 나이 지역, 성별
+	String name = "";
+	int age = 0;
+	String local = "";
+	String gender = "";
+	//생성자 선언 
+	Human(int a,int b) {
+		System.out.println("생성자 호출됨");
+	}
+}
+
