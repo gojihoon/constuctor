f924a98a906faf746508abc4bb6fe1aa45ec6ea4
gojihoon <anhj9095@naver.com>
2022-04-22 14:49:54 +0900

    constructor2

--- src/day26_20220422/Human.java
+++ src/day26_20220422/Human.java
@@ -6,9 +6,26 @@ public class Human {
 	int age = 0;
 	String local = "";
 	String gender = "";
-	//생성자 선언 
-	Human(int a,int b) {
+
+	// 생성자 선언
+	Human() {
 		System.out.println("생성자 호출됨");
 	}
-}
+	//필드값을 세팅하게 위해 매개변수가 있는 생성자 선언
+	Human(String name, int age, String local, String gender){
+		System.out.println("매개변수 있는 생성자");
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(local);
+		System.out.println(gender);
+	}
 
+	// 매개변수가 있는 생성자(여러개 가능, 타입이 같게는 여러개 불가
+	Human(String str1) {
+		System.out.println("넘겨받은 값은 " + str1);
+	}
+	//int 매개변수 생성자
+	Human(int int1) {
+		System.out.println("넘겨받은 값은 " + int1);
+	}
+}
--- src/day26_20220422/HumanMain.java
+++ src/day26_20220422/HumanMain.java
@@ -4,14 +4,24 @@ public class HumanMain {
 
 	public static void main(String[] args) {
 		// Human 클래스에 대한 객체(인스턴스) 생성
-		Human human1 = new Human(20,30);
+		Human human1 = new Human();
 		// [클래스 이름] [객체이름(내맘대로)] = new [클래스 생성자]
 		// human1 객체에 정보를 담아봅시다.
 		System.out.println(human1.name);
 		human1.name = "홍길동";
 		System.out.println(human1.name);
-		Human human2 = new Human(10,20);
+		Human human2 = new Human();
 		System.out.println(human2.name);
+		
+		Human human3 = new Human("가니?");
+		
+		Human human4 = new Human(19);
+		
+		String var1 = "ggg";
+		Human human5 = new Human(var1);
+		
+		//human6 객체 ,정확한 순서와 타입이 맞아야 한다.
+		Human human6 = new Human("고지훈",20,"인천","남");
 	}
 
 }
